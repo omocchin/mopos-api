0536c4b1014122bfc5efe9eabdcfe28020d4d67e
Kyle Mitsui <omocchin@gmail.com>
2024-01-30 23:07:00 +0900

    add authority to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
 company = Company.create(company_uuid: 'abcd1234', login_id: 'mipos', name: 'Mipos Co.', password: 'aaaaaaaa', email: 'test@example.com', tel: '1111111111')
 
 authority = UserAuthority.create(name: 'master')
+UserAuthority.create(name: 'company_master')
+UserAuthority.create(name: 'comapny_manager')
+UserAuthority.create(name: 'worker')
 
 user = User.create(company_id: company.id, user_authority_id: authority.id, login_id: 'test1234', password: 'aaaaaaaa', user_number: 11111111, first_name: 'Tarou', last_name: 'Test')
 pat = Pay.create(user_id: user.id, hourly_rate: 10.00)
